153568b6846588a578cdcadae3bc673636d37195
tuanla <timlaibautroi2k@gmail.com>
2022-06-18 11:51:47 +0700

    validation unique studentId, email, phone onBlur

--- src/Pages/Admin/addUser/index.js
+++ src/Pages/Admin/addUser/index.js
@@ -18,25 +18,114 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import Alert from '@mui/material/Alert';
 import Snackbar from '@mui/material/Snackbar';
+import { DatePicker, LocalizationProvider, TimePicker } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { vi } from 'date-fns/locale';
+import moment from 'moment';
 
 function AddUser() {
     const [openSnackBar, setOpenSnackBar] = useState(false);
+    const [listStudentId, setListStudentId] = useState();
+    const [listEmail, setListEmailId] = useState();
+    const [listPhone, setListPhone] = useState();
     let snackBarStatus;
 
+    useEffect(() => {
+        const fetchUserList = async () => {
+            try {
+                const response = await userApi.getAll();
+                console.log(response);
+                let listStudentId = response.data.map((i) => i.studentId);
+                let listEmail = response.data.map((i) => i.email);
+                let listPhone = response.data.map((i) => i.phone);
+
+                setListStudentId(listStudentId);
+                setListEmailId(listEmail);
+                setListPhone(listPhone);
+                console.log(listStudentId, listEmail, listPhone);
+            } catch (error) {
+                console.log('Failed to fetch user list: ', error);
+            }
+        };
+        fetchUserList();
+    }, []);
+
+    function uniqueStudentId(message) {
+        return this.test('uniqueStudentId', message, function (value) {
+            let filterStudentId = listStudentId.filter((i) => i === value);
+            const { path, createError } = this;
+
+            if (filterStudentId.length === 1) {
+                return createError({ path, message: message ?? `MSV "${value}" đã tồn tại` });
+            }
+            if (!value) {
+                return createError({ path, message: 'Không được để trống trường này' });
+            }
+
+            if (!value.match(/[A-Z]{2}\d{6}/)) {
+                return createError({
+                    path,
+                    message: 'Vui lòng nhập đúng định dạng',
+                });
+            }
+
+            return true;
+        });
+    }
+    function uniqueEmail(message) {
+        return this.test('uniqueEmail', message, function (value) {
+            let filterEmail = listEmail.filter((i) => i === value);
+            const { path, createError } = this;
+
+            if (filterEmail.length === 1) {
+                return createError({ path, message: message ?? `Email "${value}" đã tồn tại` });
+            }
+            if (!value) {
+                return createError({ path, message: 'Không được để trống trường này' });
+            }
+
+            if (!value.match(/^[\w]+@(fpt.edu.vn)/)) {
+                return createError({
+                    path,
+                    message: 'Vui lòng nhập email FPT',
+                });
+            }
+
+            return true;
+        });
+    }
+    function uniquePhone(message) {
+        return this.test('uniquePhone', message, function (value) {
+            let filterPhone = listPhone.filter((i) => i === value.toString());
+            const { path, createError } = this;
+
+            if (filterPhone.length === 1) {
+                return createError({ path, message: message ?? `Số điện thoại "${value}" đã tồn tại` });
+            }
+            if (!value) {
+                return createError({ path, message: 'Không được để trống trường này' });
+            }
+
+            if (!value.match(/(84|0[3|5|7|8|9])+([0-9]{8})\b/)) {
+                return createError({
+                    path,
+                    message: 'Vui lòng nhập đúng số điện thoại',
+                });
+            }
+
+            return true;
+        });
+    }
+    Yup.addMethod(Yup.mixed, 'uniqueStudentId', uniqueStudentId);
+    Yup.addMethod(Yup.mixed, 'uniqueEmail', uniqueEmail);
+    Yup.addMethod(Yup.mixed, 'uniquePhone', uniquePhone);
     const validationSchema = Yup.object().shape({
         name: Yup.string().required('Không được để trống trường này'),
-        studentId: Yup.string()
-            .required('Không được để trống trường này')
-            .matches(/[A-Z]{2}\d{6}/, 'Nhập đúng định dạng'),
-
+        studentId: Yup.mixed().uniqueStudentId(),
         dateOfBirth: Yup.string().required('Không được để trống trường này'),
-        email: Yup.string()
-            .required('Không được để trống trường này')
-            .matches(/^[\w]+@(fpt.edu.vn)/, 'Nhập mail FPT'),
+        email: Yup.mixed().uniqueEmail(),
         gender: Yup.string().required('Không được để trống trường này'),
-        phone: Yup.string()
-            .required('Không được để trống trường này')
-            .matches(/(84|0[3|5|7|8|9])+([0-9]{8})\b/),
+        phone: Yup.mixed().uniquePhone(),
         roleId: Yup.string().required('Không được để trống trường này'),
     });
 
@@ -67,7 +156,17 @@ function AddUser() {
         }
     };
     const onSubmit = async (data) => {
-        await userApi.createUser(data).then((res) => {
+        const dataFormat = {
+            currentAddress: 'Dom D',
+            dateOfBirth: moment(new Date(data.dateOfBirth)).format('yyyy-MM-DD'),
+            email: data.email,
+            gender: data.gender,
+            name: data.name,
+            phone: data.phone,
+            roleId: data.roleId,
+            studentId: data.studentId,
+        };
+        await userApi.createUser(dataFormat).then((res) => {
             console.log('1', res);
             console.log('2', res.data);
             if (res.data.length != 0) {
@@ -141,7 +240,7 @@ function AddUser() {
                             error={errors.studentId ? true : false}
                             helperText={errors.studentId?.message}
                         />
-                        <TextField
+                        {/* <TextField
                             required
                             id="outlined-disabled"
                             label="Ngày sinh"
@@ -149,7 +248,40 @@ function AddUser() {
                             {...register('dateOfBirth')}
                             error={errors.dateOfBirth ? true : false}
                             helperText={errors.dateOfBirth?.message}
-                        />
+                        /> */}
+                        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={vi}>
+                            <Controller
+                                required
+                                name="dateOfBirth"
+                                control={control}
+                                defaultValue={null}
+                                render={({ field: { onChange, value }, fieldState: { error, invalid } }) => (
+                                    <DatePicker
+                                        label="Ngày sinh"
+                                        // views={['year', 'month', 'day']}
+                                        disableFuture
+                                        // maxDate={new Date('2022-06-12')}
+                                        views={['year', 'month', 'day']}
+                                        ampm={false}
+                                        value={value}
+                                        onChange={(value) => onChange(value)}
+                                        renderInput={(params) => (
+                                            <TextField
+                                                {...params}
+                                                required
+                                                id="outlined-disabled"
+                                                error={invalid}
+                                                helperText={invalid ? error.message : null}
+                                                // id="startDate"
+                                                variant="outlined"
+                                                margin="dense"
+                                                fullWidth
+                                            />
+                                        )}
+                                    />
+                                )}
+                            />
+                        </LocalizationProvider>
 
                         <TextField
                             required
