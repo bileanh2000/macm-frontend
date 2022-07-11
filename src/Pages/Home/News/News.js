bb0d5008be4e3db5f605ad4ad15978f55b14a69f
tungdt1412 <tungdt1412@gmail.com>
2022-07-11 21:16:24 +0700

    Chỉnh sửa giao diện trang home

--- src/Pages/Home/News/News.js
+++ src/Pages/Home/News/News.js
@@ -1,37 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import classNames from 'classnames/bind'
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames/bind';
 
-import styles from './News.module.scss'
-import { Grid } from '@mui/material'
+import styles from './News.module.scss';
+import { Grid } from '@mui/material';
 import adminNewsAPI from 'src/api/adminNewsAPI';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function News() {
-    const [newsList, setNews] = useState([])
+    const [newsList, setNews] = useState([]);
 
     const fetchNewsList = async () => {
         try {
-            const response = await adminNewsAPI.getNews()
+            const response = await adminNewsAPI.getNews();
             console.log(response);
-            setNews(response.data)
+            setNews(response.data);
         } catch (error) {
-            console.log('Lấy dữ liệu news thất bại')
+            console.log('Lấy dữ liệu news thất bại');
         }
-    }
+    };
 
     useEffect(() => {
-        fetchNewsList()
-    }, [])
+        fetchNewsList();
+    }, []);
 
     return (
         <div className={cx('news-container')}>
-            <h2>News</h2>
+            <h2>Thông báo</h2>
 
             {newsList.map((news, index) => (
                 <div className={cx('news-item')} key={index}>
                     <Grid container spacing={1} className={cx('news-title')}>
-                        <Grid item xs={10}><h3> * {news.title}</h3></Grid>
+                        <Grid item xs={10}>
+                            <h3> * {news.title}</h3>
+                        </Grid>
                         {/* <Grid item xs={2} className={cx('news-tag')} >
                             <small>{news.tag}</small>
                         </Grid> */}
@@ -43,7 +45,7 @@ function News() {
             ))}
             <small className={cx('news-link')}>Xem thêm</small>
         </div>
-    )
+    );
 }
 
-export default News
+export default News;
