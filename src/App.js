c4f0a536b0365b5eb86deb11fed6271ed495ebb3
tuanla <timlaibautroi2k@gmail.com>
2022-07-19 21:42:13 +0700

    fix snackbar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { getCurrentUser } from './api/APIUtils';
 
 import LoadingProgress from './Components/LoadingProgress';
 import { ACCESS_TOKEN } from './constants';
-import PrivateRoute from './Components/Common/PrivateRoute';
+// import PrivateRoute from './Components/Common/PrivateRoute';
 import OAuth2RedirectHandler from './oauth2/OAuth2RedirectHandler';
 import Home from './Pages/Admin/Home';
 import ErrorPage from './Pages/ErrorPage';
@@ -126,74 +126,76 @@ class App extends Component {
         }
 
         return (
-            <div className="app">
-                <Routes>
-                    <Route
-                        path="/"
-                        // render={(props) => <Login authenticated={this.state.authenticated} {...props} />}
-                        element={this.state.authenticated ? <Navigate to="/home" /> : <Login />}
-                        // element={<Login />}
-                    ></Route>
-                    <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />}></Route>
-                    {userRouter.map((route, index) => {
-                        const Page = route.component;
-                        let Layout = DefaultLayout;
-                        if (route.layout) {
-                            Layout = route.layout;
-                        } else if (route.layout === null) {
-                            Layout = Fragment;
-                        }
+            <SnackbarProvider maxSnack={4} autoHideDuration={5000} preventDuplicate>
+                <div className="app">
+                    <Routes>
+                        <Route
+                            path="/"
+                            // render={(props) => <Login authenticated={this.state.authenticated} {...props} />}
+                            element={this.state.authenticated ? <Navigate to="/home" /> : <Login />}
+                            // element={<Login />}
+                        ></Route>
+                        <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />}></Route>
+                        {userRouter.map((route, index) => {
+                            const Page = route.component;
+                            let Layout = DefaultLayout;
+                            if (route.layout) {
+                                Layout = route.layout;
+                            } else if (route.layout === null) {
+                                Layout = Fragment;
+                            }
 
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    localStorage.getItem(ACCESS_TOKEN) ? (
-                                        <Layout onLogout={this.handleLogout}>
-                                            <Page />
-                                        </Layout>
-                                    ) : (
-                                        <Navigate to="/" />
-                                    )
-                                }
-                            />
-                        );
-                    })}
-                    {this.isAdmin()
-                        ? adminRouters.map((route, index) => {
-                              const Page = route.component;
-                              let Layout = DefaultLayout;
-                              if (route.layout) {
-                                  Layout = route.layout;
-                              } else if (route.layout === null) {
-                                  Layout = Fragment;
-                              }
+                            return (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    element={
+                                        localStorage.getItem(ACCESS_TOKEN) ? (
+                                            <Layout onLogout={this.handleLogout}>
+                                                <Page />
+                                            </Layout>
+                                        ) : (
+                                            <Navigate to="/" />
+                                        )
+                                    }
+                                />
+                            );
+                        })}
+                        {this.isAdmin()
+                            ? adminRouters.map((route, index) => {
+                                  const Page = route.component;
+                                  let Layout = DefaultLayout;
+                                  if (route.layout) {
+                                      Layout = route.layout;
+                                  } else if (route.layout === null) {
+                                      Layout = Fragment;
+                                  }
 
-                              return (
-                                  <Route
-                                      key={index}
-                                      path={route.path}
-                                      element={
-                                          localStorage.getItem(ACCESS_TOKEN) ? (
-                                              <Layout onLogout={this.handleLogout}>
-                                                  <Page />
-                                              </Layout>
-                                          ) : (
-                                              <Navigate to="/" />
-                                          )
-                                      }
-                                  />
-                              );
-                          })
-                        : null}
-                </Routes>
-                {/* {console.log(this.isAdmin())} */}
-                {/* {alert(JSON.parse(localStorage.getItem('currentUser')).role.id)} */}
-                {/* {window.location.pathname.startsWith('/admin') ? <Navigate to="/forbidden" /> : ''} */}
+                                  return (
+                                      <Route
+                                          key={index}
+                                          path={route.path}
+                                          element={
+                                              localStorage.getItem(ACCESS_TOKEN) ? (
+                                                  <Layout onLogout={this.handleLogout}>
+                                                      <Page />
+                                                  </Layout>
+                                              ) : (
+                                                  <Navigate to="/" />
+                                              )
+                                          }
+                                      />
+                                  );
+                              })
+                            : null}
+                    </Routes>
+                    {/* {console.log(this.isAdmin())} */}
+                    {/* {alert(JSON.parse(localStorage.getItem('currentUser')).role.id)} */}
+                    {/* {window.location.pathname.startsWith('/admin') ? <Navigate to="/forbidden" /> : ''} */}
 
-                {/* <Alert stack={{ limit: 3 }} timeout={3000} position="top-right" effect="slide" offset={65} /> */}
-            </div>
+                    {/* <Alert stack={{ limit: 3 }} timeout={3000} position="top-right" effect="slide" offset={65} /> */}
+                </div>
+            </SnackbarProvider>
         );
     }
 }
