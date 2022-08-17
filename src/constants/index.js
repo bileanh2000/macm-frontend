e8fe16d92a6a050e5a05d8b89725d192ca1f0178
toandam0210 <84827568+toandam0210@users.noreply.github.com>
2022-08-18 05:21:35 +0700

    config to deploy

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,8 +7,8 @@ export const ACCESS_TOKEN = 'accessToken';
 export const GOOGE_LOGO =
     'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/300px-Google_%22G%22_Logo.svg.png';
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect';
-// export const OAUTH2_REDIRECT_URI = 'https://macm-seven.vercel.app/oauth2/redirect';
+//export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect';
+ export const OAUTH2_REDIRECT_URI = 'https://macm-vip.herokuapp.com/oauth2/redirect';
 
 export const GOOGLE_AUTH_URL =
     'https://capstone-project-macm.herokuapp.com' + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
