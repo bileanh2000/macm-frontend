90b301b2803568387bb470e1b04657f52ebb594e
tuanla <timlaibautroi2k@gmail.com>
2022-08-30 22:09:55 +0700

    fix config

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,4 +11,4 @@ export const GOOGE_LOGO =
 export const OAUTH2_REDIRECT_URI = 'https://macm-frontend.vercel.app/oauth2/redirect';
 
 export const GOOGLE_AUTH_URL =
-    'https://capstone-project-macm.herokuapp.com' + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
+    'https://fpt-macm.herokuapp.com' + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
